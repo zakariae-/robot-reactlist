7936986bd46fcd164178c10d9e61c140e3b8926a
zakariae <zakariae@example.com>
2019-07-21 10:14:48 +0200

    Updates

---
+++ precache-manifest.e5bdcd751385b3283313bf8e51d4aedf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6362656c014e7663510d654e0c5f17",
+    "url": "/robot-reactlist/index.html"
+  },
+  {
+    "revision": "6f0fa3b1db0b9c5ffce6",
+    "url": "/robot-reactlist/static/css/main.e290888d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/robot-reactlist/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6f0fa3b1db0b9c5ffce6",
+    "url": "/robot-reactlist/static/js/main.c477bf96.chunk.js"
+  },
+  {
+    "revision": "39369a694e33bc00a3ad",
+    "url": "/robot-reactlist/static/js/runtime~main.ea906c6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robot-reactlist/static/media/logo.5d5d9eef.svg"
+  }
+]);
